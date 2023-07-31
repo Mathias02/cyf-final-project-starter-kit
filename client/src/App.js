5e214f3364456561d8e88d29b8244c98e3526c61
EleMashige <elekanyanimashige@yahoo.com>
2023-07-31 13:44:30 +0200

    created trainee dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import Navbar from './Components/Navbar/Navbar';
-// import '/Users/admin/Desktop/Newsletter-Signup/cyf-final-project-starter-kit/client/src/Components/Navbar/Navbar.css';
-// import './App.css';
-// import Home from './Components/Home Page/Home';
-
-// const App = () => {
-//   return (
-// 	<div>
-// 	<Home />
-// 	</div>
-
-//   );
-// };
-
-// export default App;
 
 import React from 'react';
 import './App.css';
@@ -21,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home Page/Home';
 import TraineeLogin from './Components/Login/TraineeLogin';
 import AdminLogin from './Components/Login/AdminLogin';
+import TraineeTracker from './Components/Trainee Dashboard/TraineeTracker'
 
 const App = () => {
   return (
@@ -30,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/trainee-login" element={<TraineeLogin />} />
           <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/trainee-tracker" element={<TraineeTracker />} />
         </Routes>
       </div>
   );
