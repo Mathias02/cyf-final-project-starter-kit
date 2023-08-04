d19b700202e4ea948880608ecb444434ebdbd0c3
EleMashige <elekanyanimashige@yahoo.com>
2023-08-04 19:57:42 +0200

    created post request

--- client/src/Components/Admin Dashboard/Modules.js
+++ client/src/Components/Admin Dashboard/Modules.js
@@ -1,15 +1,65 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
+import React, { useState } from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import "./modules.css";
+import axios from "axios"; // Change import statement here
 
 const Modules = () => {
-    return(
-        <div>
-        <Navbar />
-            <h1>Hello vlounteer, below below you can update modules</h1>
-            <Footer />
-        </div>
-    )
-}
+  // STATE
+  const [moduleName, setModuleName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [cohortName, setCohortName] = useState("");
+
+  // SUBMIT FUNCTION
+  const insertModule = async (data) => {
+    try {
+      const response = await axios.post("/modules", data);
+      console.log("Data inserted successfully!");
+      // Do something with the response if needed
+    } catch (error) {
+      console.error("Error inserting data:", error);
+      // Handle the error
+
+      if (error.response) {
+        console.error("Response data:", error.response.data);
+        console.error("Response status:", error.response.status);
+        console.error("Response headers:", error.response.headers);
+      } else if (error.request) {
+        console.error("No response received:", error.request);
+      } else {
+        console.error("Error setting up the request:", error.message);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <h3>Hello volunteer, below you can update modules</h3>
+      <div className='form'>
+        <input type='text' placeholder='Enter Module Name' name='moduleName' onChange={(e) => {
+          setModuleName(e.target.value);
+        }} />
+        <input type='date' placeholder='Enter Start Date' name='startDate' onChange={(e) => {
+          setStartDate(e.target.value);
+        }} />
+        <input type='date' placeholder='Enter End Date' name='endDate' onChange={(e) => {
+          setEndDate(e.target.value);
+        }} />
+        <input type='text' placeholder='Enter Cohort Name' name='cohortName' onChange={(e) => {
+          setCohortName(e.target.value);
+        }} />
+        <button onClick={() => insertModule({
+          moduleName: moduleName,
+          startDate: startDate,
+          endDate: endDate,
+          cohort: cohortName,
+        })}> Submit </button>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Modules;
--- server/api.js
+++ server/api.js
@@ -1,28 +1,53 @@
 import { Router } from "express";
+import express from "express";
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
 import db from "./db";
 import logger from "./utils/logger";
 
 const router = Router();
+// app.use(express.json());
 
-const fetchDataFromDb = async () => {
-  try {
-    const sqlInsert =
-      "INSERT INTO modules (modulename, startdate, enddate, cohort) VALUES ('Ele', '2023-12-01', '2023-12-01', 'South Africa');";
-    const result = await db.query(sqlInsert);
-    logger.debug("Data inserted successfully!");
-    // Do something with the result if needed
-  } catch (err) {
-    logger.error("%O", err);
-    throw err; // Rethrow the error to handle it later
-  }
-};
+//POST
 
-// Call the function to fetch data from the database
-fetchDataFromDb();
+router.post("/modules", async (req, res) => {
+	try {
+	  const { title, startDate, endDate, location, cohort } = req.body;
+	  const query = `
+		INSERT INTO modules (title, start_date, end_date, location, cohort)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING *`;
+	  const values = [title, startDate, endDate, location, cohort];
+	  const result = await pool.query(query, values);
+	  res.status(201).json(result.rows[0]);
+	} catch (error) {
+	  logger.error("Error adding module:", error);
+	  res.status(500).json({ error: "Internal Server Error" });
+	}
+  });
+
+
+// router.post("/modules", async (req, res) => {
+// 	const { moduleName, startDate, endDate, cohort } = req.body;
+// 	try {
+// 	  await db(moduleName, startDate, endDate, cohort);
+// 	  res.json({ message: "Data inserted successfully!" });
+// 	} catch (err) {
+// 	  console.error("Error inserting data:", err);
+// 	  res.status(500).json({ error: "Error inserting data." });
+// 	}
+//   });
+
 
 router.get("/", (_, res) => {
   logger.debug("Welcoming everyone...");
   res.json({ message: "Hello, world!" });
 });
 
+
+
+app.listen(3036, () => {
+	console.log("Server running on http://localhost:3000");
+  });
 export default router;
