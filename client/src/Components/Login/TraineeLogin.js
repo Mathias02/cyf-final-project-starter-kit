c97714671f459b1dc684151e803ea41caf352a5d
Mathias02 <matthiasm2016@gmail.com>
2023-08-10 01:11:28 +0200

    initial commit

--- client/src/Components/Login/TraineeLogin.js
+++ client/src/Components/Login/TraineeLogin.js
@@ -3,23 +3,38 @@ import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import './TraineeLogin.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import TraineeTracker from '../Trainee Dashboard/TraineeTracker';
 
 const TraineeLogin = () => {
+  const [username, setUsername] = useState('');
+  const [show, setShow] = useState(false);
+
+const userInput = (e) =>{
+   e.preventDefault();
+   console.log(username);
+}
+
+if(show) {
+  return(
+    <TraineeTracker  />
+  ) 
+}
   return (
     <div>
-    <Navbar />
-    <div className='traine-login-container'>
-    <div>
-    <img className='signin-logo' src="https://syllabus.codeyourfuture.io/img/logo.png" alt="Your Logo" />
-    </div>
-    <Link to='/trainee-tracker'>
-    <div className="github-sign-in-button" onClick={() => console.log('Sign in with GitHub clicked!')}>
-          <i className="fab fa-github"></i> {/* GitHub icon */}
-          <span>Sign in with GitHub</span>
-    </div>
-    </Link>
-    </div>
+      <Navbar />
+      <div className='trainee-login-container'>
+        <div className="sign">
+            <div className="github">
+              <i className="fab fa-github"></i> {/* GitHub icon */}
+                <span>Sign in with GitHub</span>
+            </div>
+            <form className="myform">
+              <input type="text" id='userdetails' value={username} className='user'  name='userdetails' placeholder='Github username' onChange={e => setUsername(e.target.value)} required/>
+              <input type="submit" className='btn-submit' value="submit" onClick={() => setShow(!show)} />
+            </form>
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- client/src/Components/Trainee Dashboard/TraineeTracker.js
+++ client/src/Components/Trainee Dashboard/TraineeTracker.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import './TraineeTracker.css';
 
-const TraineeTracher = () => {
+const TraineeTracker = () => {
+
+
+    useEffect(() =>{
+        fetch('https://api.github.com/search/issues?q=is:pr%20author:Mathias02%20user:codeyourfuture')
+        .then(res => console.log(res.json()))
+    },[]);
+
     return(
-        <div>
+        <div className='tracker'>
         <Navbar />
-            <h1>Hello trainee, below is your tracker</h1>
-            <Footer />
+            <h1>Hello trainee, below is your tracked score</h1>
+            <div className='tabcontainer'>
+                <table className='tab'>
+                    <thead>
+                        <tr>
+                            <th>PRs</th>
+                            <th>CodeWars</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>21</td>
+                            <td>12</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        <Footer />
         </div>
     )
 }
 
-export default TraineeTracher;
+export default TraineeTracker;
