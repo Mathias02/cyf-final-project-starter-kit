15c0dc9779b2d07ba9eee69bb4796b0b5a9674b6
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-12 18:24:26 +0100

    removed the conflicts on trainee traker file

--- client/src/Components/Trainee Dashboard/TraineeTracker.js
+++ client/src/Components/Trainee Dashboard/TraineeTracker.js
@@ -1,63 +1,71 @@
-
 import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import "./TraineeTracker.css";
 
 const TraineeTracker = ({ user }) => {
+    const [githubData, setGithubData] = useState(null);
+    const [codewars, setCodewars] = useState(null);
+    const [cohort, setCohort] = useState(null);
 
-const [entry, setEntry] = useState([]);
-const [codewars, setCodewars] = useState([]);
-
-    useEffect(() =>{
+    useEffect(() => {
         fetch(`https://api.github.com/search/issues?q=is:pr%20author:${user}%20user:codeyourfuture`)
-        .then((res) => {
-            if(res.status == 200){
-               return res.json();
-            }else{
-                console.log("wrong username");
-            }
+            .then((res) => {
+                if (res.status === 200) {
+                    return res.json();
+                } else {
+                    console.log("Wrong username or other error with the GitHub API.");
+                    throw new Error("Failed to fetch from GitHub");
+                }
             })
-        .then((data) => setEntry(data));
+            .then((data) => setGithubData(data)); // Fixed from setEntry to setGithubData
     },[user]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch(`https://www.codewars.com/api/v1/users/${user}`)
-        .then((res) => res.json())
-        .then((data) => setCodewars(data));
-    },[user]);
+            .then((res) => res.json())
+            .then((data) => setCodewars(data))
+            .catch((error) => console.error("Error fetching data from CodeWars:", error));
+    }, [user]);
 
-    // fetch api cohorts
+    useEffect(() => {
+        // Fetch cohort information for the given user (GitHub username)
+        fetch(`/api/trainees?github_name=${user}`)
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.length > 0 && data[0].cohort) {
+                    // Set the cohort state with the cohort information
+                    setCohort(data[0].cohort);
+                } else {
+                    console.log("No cohort data found for this user.");
+                }
+            })
+            .catch((error) => console.error("Error fetching cohort data:", error));
+    }, [user]);
 
-useEffect(() => {
-    fetch(`/api/cohorts/${user}`)
-        .then((res) => res.json())
-        .then((data) => setEntry(data));
-}, [user]);
-
-
-
-    return(
+    return (
         <div className="tracker">
-        <Navbar />
+            <Navbar />
             <h1>Hello {user}, below is your tracked score</h1>
             <div className="tabcontainer">
                 <table className="tab">
                     <thead>
                         <tr>
-                            <th>PRs</th>
-                            <th>CodeWars</th>
+                            <th>GitHub PRs</th>
+                            <th>CodeWars Honor</th>
+                            <th>Cohort Name</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>{entry?.total_count || "Loading..."}</td>
+                            <td>{githubData?.total_count || "Loading..."}</td>
                             <td>{codewars?.honor || "Loading..."}</td>
+                            <td>{cohort?.name || "Loading..."}</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-        <Footer />
+            <Footer />
         </div>
     );
 };
