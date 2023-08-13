726483d3f5747f23024abf38854e3da6557ac16d
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-13 16:48:30 +0100

    fetch api trainee related cohorts

--- client/src/Components/Trainee Dashboard/TraineeTracker.js
+++ client/src/Components/Trainee Dashboard/TraineeTracker.js
@@ -7,41 +7,53 @@ const TraineeTracker = ({ user }) => {
     const [githubData, setGithubData] = useState(null);
     const [codewars, setCodewars] = useState(null);
     const [cohort, setCohort] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch(`https://api.github.com/search/issues?q=is:pr%20author:${user}%20user:codeyourfuture`)
-            .then((res) => {
-                if (res.status === 200) {
-                    return res.json();
-                } else {
-                    console.log("Wrong username or other error with the GitHub API.");
-                    throw new Error("Failed to fetch from GitHub");
-                }
-            })
-            .then((data) => setGithubData(data)); // Fixed from setEntry to setGithubData
-    },[user]);
+        Promise.all([
+            fetch(`https://api.github.com/search/issues?q=is:pr%20author:${user}%20user:codeyourfuture`)
+                .then((res) => {
+                    if (res.status === 200) {
+                        return res.json();
+                    } else {
+                        console.log("Wrong username or other error with the GitHub API.");
+                        throw new Error("Failed to fetch from GitHub");
+                    }
+                })
+                .then((data) => setGithubData(data)),
 
-    useEffect(() => {
-        fetch(`https://www.codewars.com/api/v1/users/${user}`)
-            .then((res) => res.json())
-            .then((data) => setCodewars(data))
-            .catch((error) => console.error("Error fetching data from CodeWars:", error));
+            fetch(`https://www.codewars.com/api/v1/users/${user}`)
+                .then((res) => res.json())
+                .then((data) => setCodewars(data))
+                .catch((error) => {
+                    console.error("Error fetching data from CodeWars:", error);
+                    setError("There was an error fetching your CodeWars data.");
+                }),
+
+            fetch(`/api/trainees?github_name=${user}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.length > 0 && data[0].cohort) {
+                        setCohort(data[0].cohort);
+                    } else {
+                        console.log("No cohort data found for this user.");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error fetching cohort data:", error);
+                    setError("There was an error fetching your cohort data.");
+                }),
+        ])
+        .finally(() => setLoading(false));
     }, [user]);
 
-    useEffect(() => {
-        // Fetch cohort information for the given user (GitHub username)
-        fetch(`/api/trainees?github_name=${user}`)
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.length > 0 && data[0].cohort) {
-                    // Set the cohort state with the cohort information
-                    setCohort(data[0].cohort);
-                } else {
-                    console.log("No cohort data found for this user.");
-                }
-            })
-            .catch((error) => console.error("Error fetching cohort data:", error));
-    }, [user]);
+    if (loading) {
+return <p>Loading...</p>;
+}
+    if (error) {
+return <p className="error-message">{error}</p>;
+}
 
     return (
         <div className="tracker">
@@ -60,7 +72,7 @@ const TraineeTracker = ({ user }) => {
                         <tr>
                             <td>{githubData?.total_count || "Loading..."}</td>
                             <td>{codewars?.honor || "Loading..."}</td>
-                            <td>{cohort?.name || "Loading..."}</td>
+                            <td>{user || "Loading..."}</td>
                         </tr>
                     </tbody>
                 </table>
