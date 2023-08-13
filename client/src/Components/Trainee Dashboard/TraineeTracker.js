d019638c9593dc8acd91e4c3a9d1b7120e81bdc0
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-13 20:34:46 +0100

    to change branch

--- client/src/Components/Trainee Dashboard/TraineeTracker.js
+++ client/src/Components/Trainee Dashboard/TraineeTracker.js
@@ -1,49 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
-import './TraineeTracker.css';
+import React, { useEffect, useState } from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import "./TraineeTracker.css";
 
-const TraineeTracker = ({user}) => {
+const TraineeTracker = ({ user }) => {
 
 const [entry, setEntry] = useState([]);
 const [codewars, setCodewars] = useState([]);
 
     useEffect(() =>{
         fetch(`https://api.github.com/search/issues?q=is:pr%20author:${user}%20user:codeyourfuture`)
-        .then(res => {
+        .then((res) => {
             if(res.status == 200){
-               return res.json()
+               return res.json();
             }else{
-                console.log('wrong username')
+                console.log("wrong username");
             }
             })
-        .then(data => setEntry(data)
-      },[user]);
+        .then((data) => setEntry(data));
+    },[user]);
 
     useEffect(() =>{
         fetch(`https://www.codewars.com/api/v1/users/${user}`)
-        .then(res => res.json())
-        .then(data => setCodewars(data))
+        .then((res) => res.json())
+        .then((data) => setCodewars(data));
     },[user]);
 
-    useEffect(() => {
-        fetch(`/api/trainees?github_name=${user}`)
-          .then(res => res.json())
-          .then(data => {
-            if (data.length > 0 && data[0].cohort) {
-              setCohort(data[0].cohort);
-            } else {
-              console.log('No cohort data found for this user.');
-            }
-            setLoading(false);
-          })
-          .catch(err => {
-            console.error('Error fetching cohort data:', err);
-            setError(err.message || 'An error occurred while fetching cohort data.');
-            setLoading(false);
-          });
-      }, [user]);
-    
+
     return(
         <div className='tracker'>
         <Navbar />
@@ -58,8 +41,8 @@ const [codewars, setCodewars] = useState([]);
                     </thead>
                     <tbody>
                         <tr>
-                            <td>{entry?.total_count || 'Loading...'}</td>
-                            <td>{codewars?.honor || 'Loading...'}</td>
+                            <td>{entry?.total_count || "Loading..."}</td>
+                            <td>{codewars?.honor || "Loading..."}</td>
                         </tr>
                     </tbody>
                 </table>
