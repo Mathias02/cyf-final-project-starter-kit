3d3b1d3d657258cc122e92ca68ab98a743724bd7
Mathias02 <matthiasm2016@gmail.com>
2023-08-14 15:25:58 +0200

    jsx created for admin trainees tracking page

--- client/src/Components/Admin Dashboard/TrackProgress.js
+++ client/src/Components/Admin Dashboard/TrackProgress.js
@@ -11,14 +11,14 @@ const TrackProgress = () => {
                 <h1>Hello volunteer, below you can track trainee progress</h1>
                 <div className="progress_container">
                     <form action="POST" className="trainee_form">                
-                        <label htmlFor="">Cohort Name: 
-                          <input type="text" className="cohort_name" name="trainee" placeholder="Enter Cohort Name" required/>
+                        <label htmlFor="cohort">Cohort Name: 
+                          <input type="text" id="cohort" className="cohort_name" name="cohort" placeholder="Enter Cohort Name" required/>
                         </label>
-                        <label htmlFor="">Start Date: 
-                          <input type="text" className="trainee_name" name='trainee' placeholder="Enter Start Date" required />
+                        <label htmlFor="startDate">Start Date: 
+                          <input type="text" id="startDate" className="trainee_name" name='startDate' placeholder="Enter Start Date" required />
                         </label>
-                        <label htmlFor="">End Date: 
-                          <input type="text" className="trainee_name" name='trainee' placeholder="Enter End Date" required />
+                        <label htmlFor="endDate">End Date: 
+                          <input type="text" id="endDate" className="trainee_name" name='endDate' placeholder="Enter End Date" required />
                         </label>
                         <input type="submit" className="trainee_submit" value="submit" />
                     </form>
