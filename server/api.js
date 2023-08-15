a276c8eb497f3b31f46fcaa83a3deb4b61056645
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-15 11:14:32 +0100

    new endpoint milestone

--- server/api.js
+++ server/api.js
@@ -1,20 +1,22 @@
-import express from "express";
+
+import express, { Router } from "express";
+
 import cors from "cors";
 import db from "./db";
 import logger from "./utils/logger";
-import bodyParser from "body-parser";
 
 const app = express();
+const router = Router();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
+router.use(express.json());
+router.use(cors());
+router.use(express.urlencoded({ extended: true }));
 
 //GET
-app.get("/get", async (req, res) => {
+router.get("/get", async (req, res) => {
 	try {
 	const querySelect = `
 		SELECT * from modules`;
@@ -33,7 +35,7 @@ app.get("/get", async (req, res) => {
 
 
 //POST
-app.post("/insert", async (req, res) => {
+router.post("/insert", async (req, res) => {
   try {
 
 const moduleName = req.body.moduleName;
@@ -58,33 +60,52 @@ const cohort = req.body.cohort;
   }
 });
 
-app.post("/insert", async (req, res) => {
-  try {
 
-const moduleName = req.body.moduleName;
-const startDate = req.body.startDate;
-const endDate = req.body.endDate;
-const cohort = req.body.cohort;
 
-    const query = `
-      INSERT INTO modules (modulename, startdate, enddate, cohort)
-      VALUES ($1, $2, $3, $4)
-      RETURNING *`;
-    const values = [moduleName, startDate, endDate, cohort];
-    const result = await db.query(query, values);
+// Endpoint to create a new cohort
 
-    // Debugging: Log the result to see if data is being inserted correctly
-    console.log("Inserted Data:", result.rows[0]);
+router.post("/api/cohorts", (req, res) => {
+	const query = req.body;
+	const insertQuery = "INSERT INTO cohorts (name) VALUES ($1) RETURNING id";
 
-    res.status(201).json(result.rows[0]);
-  } catch (error) {
-    logger.error("Error adding module:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+	try {
+		db.query(insertQuery, [query.name])
+			.then((result) => {
+				res.json({ id: result.rows[0].id });
+			})
+			.catch((error) => {
+				logger.debug(error);
+				res
+					.status(500)
+					.json({ error: "An error occurred while inserting the cohort." });
+			});
+	} catch (error) {
+		logger.debug(error);
+		res.status(500).json({ error: "An unexpected error occurred." });
+	}
+});
+
+router.get("/api/cohorts", (req, res) => {
+	const selectQuery = "SELECT * FROM cohorts";
+	try {
+		db.query(selectQuery)
+			.then((result) => {
+				res.json(result.rows);
+			})
+			.catch((error) => {
+				logger.debug(error); // Log the error
+				res
+					.status(500)
+					.json({ error: "An error occurred while fetching cohorts." });
+			});
+	} catch (error) {
+		logger.debug(error);
+		res.status(500).json({ error: "An unexpected error occurred." });
+	}
 });
 
 //DELETE
-app.delete("/delete/:id", async (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
 	try {
 	const { id } = req.params;
 	const query = `
@@ -99,34 +120,40 @@ app.delete("/delete/:id", async (req, res) => {
   });
 
   //PUT
-  app.put("/update/:id", async (req, res) => {
+  router.put("/update/:id", async (req, res) => {
 	try {
-	  const { id } = req.params;
-	  const { moduleName, startDate, endDate, cohort } = req.body;
+       const { id } = req.params;
+       const { moduleName, startDate, endDate, cohort } = req.body;
 
-	  const query = `
+        const query = `
 		UPDATE modules
 		SET modulename = $1, startdate = $2, enddate = $3, cohort = $4
 		WHERE id = $5
 		RETURNING *`;
-	  const values = [moduleName, startDate, endDate, cohort, id];
-	  const result = await db.query(query, values);
+     const values = [moduleName, startDate, endDate, cohort, id];
+      const result = await db.query(query, values);
 
-	  res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows[0]);
 	} catch (error) {
-	  logger.error("Error updating module:", error);
-	  res.status(500).json({ error: "Internal Server Error" });
+     logger.error("Error updating module:", error);
+      res.status(500).json({ error: "Internal Server Error" });
 	}
   });
+  router.post("/api/milestones", async (req, res) => {
+    const { name, date, github_pr, codewars_rank, cohort_id } = req.body;
+
+    const addMilestone = "INSERT INTO milestones (name, date, github_pr, codewars_rank, cohort_id) VALUES ($1, $2, $3, $4, $5) RETURNING id";
+
+    try {
+        const result = await db.query(addMilestone, [name, date, github_pr, codewars_rank, cohort_id]);
+        res.send(result.rows[0]);
+    } catch (error) {
+        logger.debug(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 
 
+export default router;
 
-
-
-
-
-
-
-
-export default app;
