ccaccc41be9277d016c4341daeb98cf274543ae2
Mathias02 <matthiasm2016@gmail.com>
2023-08-15 13:05:48 +0200

    css added to the admin Trackerprogress

--- client/src/Components/Admin Dashboard/TrackProgress.js
+++ client/src/Components/Admin Dashboard/TrackProgress.js
@@ -10,10 +10,9 @@ const TrackProgress = () => {
             <div className="progress_box">
                 <h1>Hello volunteer, below you can track trainee progress</h1>
                 <div className="progress_container">
-                    <form action="POST" className="trainee_form">                
-                        <label htmlFor="cohort">Cohort Name: 
-                          <input type="text" id="cohort" className="cohort_name" name="cohort" placeholder="Enter Cohort Name" required/>
-                        </label>
+                    <form className="trainee_form">                
+                        <label htmlFor="cohort">Cohort Name: </label>
+                        <input type="text" id="cohort" className="cohort_name" name="cohort" placeholder="Enter Cohort Name" require />
                         <input type="submit" className="trainee_submit" value="submit" />
                     </form>
                 </div>
