fd4c7ae5cbedf269f17fd844819527d953c69c6f
EleMashige <elekanyanimashige@yahoo.com>
2023-08-17 19:51:43 +0200

    Added filter functionality

--- client/src/Components/Admin Dashboard/Tracker Form/AdminTrackerTable.js
+++ client/src/Components/Admin Dashboard/Tracker Form/AdminTrackerTable.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import moment from 'moment';
 
 const AdminTrackerTable = () => {
     const [progressData, setProgressData] = useState([]);
@@ -10,11 +11,19 @@ const AdminTrackerTable = () => {
         codewars: "",
         cohort: ""
     });
+    const [editingEntryId, setEditingEntryId] = useState(null);
+    const [editedMilestones, setEditedMilestones] = useState("");
+    const [editedDate, setEditedDate] = useState("");
+    const [editedRequiredPullRequests, setEditedRequiredPullRequests] = useState("");
+    const [editedCodewars, setEditedCodewars] = useState("");
+    const [editedCohort, setEditedCohort] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         fetchProgressData();
     }, []);
 
+    // GET FUNCTION
     const fetchProgressData = async () => {
         try {
             const response = await axios.get("/api/traineeProgress");
@@ -32,6 +41,7 @@ const AdminTrackerTable = () => {
         }));
     };
 
+    // POST FUNCTION
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -41,36 +51,75 @@ const AdminTrackerTable = () => {
             fetchProgressData();
         } catch (error) {
             console.error("Error inserting trainee progress:", error);
-            // Handle the error, show an error message, etc.
         }
     };
 
-    const handleUpdate = async (id) => {
+    const handleEditClick = (id) => {
+        setEditingEntryId(id);
+
+        const entryToEdit = progressData.find((entry) => entry.id === id);
+
+        setEditedMilestones(entryToEdit.milestones);
+        setEditedDate(entryToEdit.date);
+        setEditedRequiredPullRequests(entryToEdit.required_pull_requests);
+        setEditedCodewars(entryToEdit.codewars);
+        setEditedCohort(entryToEdit.cohort);
+    };
+
+    const handleSaveClick = async (id) => {
         try {
-            const response = await axios.put(`/api/traineeProgress/${id}`, formData);
-            console.log("Updated row:", response.data);
+            const updatedEntry = {
+                milestones: editedMilestones,
+                date: moment(editedDate).format('YYYY-MM-DD'),
+                required_pull_requests: editedRequiredPullRequests,
+                codewars: editedCodewars,
+                cohort: editedCohort,
+            };
+            await axios.put(`/api/traineeProgress/${id}`, updatedEntry);
+            setEditingEntryId(null);
             fetchProgressData();
         } catch (error) {
-            console.error("Error updating trainee progress:", error);
-            // Handle the error, show an error message, etc.
+            console.error('Error updating trainee progress:', error);
         }
     };
 
+    // DELETE FUNCTION
     const handleDelete = async (id) => {
         try {
             await axios.delete(`/api/traineeProgress/${id}`);
             console.log("Deleted row:", id);
-            fetchProgressData(); // Fetch data again to reflect the deleted row
+            fetchProgressData();
         } catch (error) {
             console.error("Error deleting trainee progress:", error);
-            // Handle the error, show an error message, etc.
         }
     };
+
+    // FILTER
+    const handleFilterByCohort = () => {
+        const filteredData = progressData.filter((entry) => {
+            return entry.cohort.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+        setProgressData(filteredData);
+    };
+
+    const handleResetFilter = () => {
+        setSearchTerm("");
+        fetchProgressData();
+    };
+
+    useEffect(() => {
+        if (searchTerm === "") {
+            fetchProgressData();
+        } else {
+            handleFilterByCohort();
+        }
+    }, [searchTerm]);
 
     return (
         <div>
             <div>
-                <h2>Insert Trainee Progress</h2>
+                <h2>Insert new milestone</h2>
+                {/* FORM TO INSERT NEW MILESTONES */}
                 <form onSubmit={handleSubmit}>
                     <label>Milestones:</label>
                     <input type="text" name="milestones" value={formData.milestones} onChange={handleChange} /><br />
@@ -91,6 +140,17 @@ const AdminTrackerTable = () => {
                 </form>
             </div>
 
+            <div className="filter-container">
+                <input
+                    type="text"
+                    placeholder="Filter by Cohort"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button onClick={handleFilterByCohort}>Filter</button>
+                <button onClick={handleResetFilter}>Reset Filter</button>
+            </div>
+
             <div className="trainee-tracker-container">
                 <table className="tracker-table">
                     <thead>
@@ -103,17 +163,77 @@ const AdminTrackerTable = () => {
                             <th>Actions</th>
                         </tr>
                     </thead>
+                    {/* //TABLE */}
                     <tbody>
-                        {progressData.map((entry, index) => (
-                            <tr key={index}>
-                                <td>{entry.milestones}</td>
-                                <td>{new Date(entry.date).toLocaleDateString()}</td>
-                                <td>{entry.required_pull_requests}</td>
-                                <td>{entry.codewars}</td>
-                                <td>{entry.cohort}</td>
+                        {progressData.map((entry) => (
+                            <tr key={entry.id}>
                                 <td>
-                                    <button onClick={() => handleUpdate(entry.id)}>Update</button>
-                                    <button onClick={() => handleDelete(entry.id)}>Delete</button>
+                                    {editingEntryId === entry.id ? (
+                                        <input
+                                            type="text"
+                                            value={editedMilestones}
+                                            onChange={(e) => setEditedMilestones(e.target.value)}
+                                        />
+                                    ) : (
+                                        entry.milestones
+                                    )}
+                                </td>
+                                <td>
+                                    {editingEntryId === entry.id ? (
+                                        <input
+                                            type="date"
+                                            value={editedDate}
+                                            onChange={(e) => setEditedDate(e.target.value)}
+                                        />
+                                    ) : (
+                                        new Date(entry.date).toLocaleDateString()
+                                    )}
+                                </td>
+                                <td>
+                                    {editingEntryId === entry.id ? (
+                                        <input
+                                            type="number"
+                                            value={editedRequiredPullRequests}
+                                            onChange={(e) => setEditedRequiredPullRequests(e.target.value)}
+                                        />
+                                    ) : (
+                                        entry.required_pull_requests
+                                    )}
+                                </td>
+                                <td>
+                                    {editingEntryId === entry.id ? (
+                                        <input
+                                            type="number"
+                                            value={editedCodewars}
+                                            onChange={(e) => setEditedCodewars(e.target.value)}
+                                        />
+                                    ) : (
+                                        entry.codewars
+                                    )}
+                                </td>
+                                <td>
+                                    {editingEntryId === entry.id ? (
+                                        <input
+                                            type="text"
+                                            value={editedCohort}
+                                            onChange={(e) => setEditedCohort(e.target.value)}
+                                        />
+                                    ) : (
+                                        entry.cohort
+                                    )}
+                                </td>
+                                <td>
+                                    {editingEntryId === entry.id ? (
+                                        <div>
+                                            <button onClick={() => handleSaveClick(entry.id)}>Save</button>
+                                            <button onClick={() => setEditingEntryId(null)}>Cancel</button>
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            <button onClick={() => handleEditClick(entry.id)}>Edit</button>
+                                            <button onClick={() => handleDelete(entry.id)}>Delete</button>
+                                        </div>
+                                    )}
                                 </td>
                             </tr>
                         ))}
@@ -125,4 +245,7 @@ const AdminTrackerTable = () => {
 };
 
 export default AdminTrackerTable;
+
+
+
 
