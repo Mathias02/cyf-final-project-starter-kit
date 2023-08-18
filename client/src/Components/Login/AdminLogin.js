053aa414bb958bb52d52243b79ccd269d38fbff1
EleMashige <elekanyanimashige@yahoo.com>
2023-08-18 17:27:29 +0200

    Removed-admin-login

--- client/src/Components/Login/AdminLogin.js
+++ client/src/Components/Login/AdminLogin.js
@@ -21,9 +21,6 @@ const AdminLogin = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Here you can perform your authentication logic using email and password
-    // For a real application, you would typically send the data to a backend server for authentication.
-
     console.log("Email:", email);
     console.log("Password:", password);
 
@@ -38,24 +35,7 @@ const AdminLogin = () => {
 			<BackButton />
 			<div className="signin-form-container">
 				<form className="signin-form" onSubmit={handleSubmit}>
-					<div className="form-group">
-						<label>Email:</label>
-						<input
-							type="email"
-							value={email}
-							onChange={handleEmailChange}
-							required
-						/>
-					</div>
-					<div className="form-group">
-						<label>Password:</label>
-						<input
-							type="password"
-							value={password}
-							onChange={handlePasswordChange}
-							required
-						/>
-					</div>
+
 					<Link to="/admin-dashboard">
 						<button type="submit" className="signin-button">
 							Sign In
