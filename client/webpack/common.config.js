4edee0a4dad574064a7b84c91c9bde966aea76b0
Mathias02 <39374601+Mathias02@users.noreply.github.com>
2025-05-05 11:29:55 +0200

    npm installed

--- client/webpack/common.config.js
+++ client/webpack/common.config.js
@@ -1,36 +1,78 @@
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+// 	entry: "./client/src/index.js",
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.js$/,
+// 				exclude: /node_modules/,
+// 				use: {
+// 					loader: "babel-loader",
+// 					options: {
+// 						cacheDirectory: true,
+// 					},
+// 				},
+// 			},
+// 			{
+// 				test: /\.(png|svg|jpe?g|gif)$/,
+// 				loader: "file-loader",
+// 			},
+// 			{
+// 				test: /\.css$/,
+// 				use: ["style-loader", "css-loader"],
+// 			},
+// 		],
+// 	},
+// 	output: {
+// 		publicPath: "/",
+// 	},
+// 	plugins: [
+// 		new HtmlWebpackPlugin({
+// 			favicon: "./client/src/favicon.ico",
+// 			template: "./client/src/index.html",
+// 		}),
+// 	],
+// };
+
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./client/src/index.js",
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						cacheDirectory: true,
-					},
-				},
-			},
-			{
-				test: /\.(png|svg|jpe?g|gif)$/,
-				loader: "file-loader",
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
-	output: {
-		publicPath: "/",
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			favicon: "./client/src/favicon.ico",
-			template: "./client/src/index.html",
-		}),
-	],
+  entry: "./client/src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        type: 'asset/resource', // Webpack 5 approach to handle images
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"), // Output directory
+    filename: "bundle.js", // Output filename
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: "./client/src/favicon.ico",
+      template: "./client/src/index.html",
+    }),
+  ],
+  devtool: "source-map",  // Enable source maps for debugging
 };
+
